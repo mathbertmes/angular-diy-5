97aedcd61143e7492114486b5e9a46d94f4c0efa
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-13 20:56:23 -0700

    feat: increment and decrement components created

---
+++ src/app/components/decrement/decrement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-decrement',
+  standalone: true,
+  imports: [],
+  templateUrl: './decrement.component.html',
+  styleUrl: './decrement.component.css'
+})
+export class DecrementComponent {
+
+}
