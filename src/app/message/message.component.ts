120f61ef5b0722659764f9c20fe7683b0029e197
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-15 18:14:41 -0700

    message component

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css'
+})
+export class MessageComponent {
+  @Input() points: number | undefined;
+  constructor() {}
+  ngOnInit(): void {}
+}
