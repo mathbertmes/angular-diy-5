8cba0996599645d7f0b1704cbf14c3595b80184b
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-15 18:15:52 -0700

    all components added to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { IncrementComponent } from './components/increment/increment.component';
+import { DecrementComponent } from './components/decrement/decrement.component';
+import { MessageComponent } from './message/message.component';
+import { ResetComponent } from './reset/reset.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IncrementComponent, DecrementComponent, MessageComponent, ResetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-diy-5';
+  points = 0
+
+  increment(){
+    this.points = this.points + 1
+  }
+
+  decrement(){
+    this.points = this.points - 1
+  }
+
+  reset(){
+    this.points = 0
+  }
 }
